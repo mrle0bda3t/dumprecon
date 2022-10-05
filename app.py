f358ef34a5de1c6a432340ba0b177d8a8e971008
mrle0bda3t <thaibatuyen2081999@gmail.com>
2022-10-05 13:48:55 +0700

    add webprobe

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask_pymongo import PyMongo
 import threading
 from objects.Port import Port
 from objects.Subdomain import Subdomain
+import requests
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/recon'
@@ -16,11 +17,38 @@ mongo = PyMongo(app)
 
 PSfunction = CDLL("/home/r3dvv0p/Desktop/DoAn/webapp/modules/tcp_port_scanner.so")
 PSfunction.portScanner.argtypes = [POINTER(c_char), POINTER(c_int)]
-# def webProbe(subdomain):
-
+def webProbe(subdomain):
+    for port in subdomain.ports:
+        if port.name in (443,8443,80,80880):
+            if port.name==443 or port.name==8443:
+                single_entry_url = "https://" + subdomain.name+":"+str(port.name)
+            elif port.name==80 or port.name==8080:
+                single_entry_url = "http://" + subdomain.name+":"+str(port.name)
+            try:
+                response = requests.get(single_entry_url, timeout=5, allow_redirects=False)
+                if response.status_code == 200: 
+                    print(response.url + " resolved")
+                    port.setUrl(response.url)
+                elif (response.status_code == 301) or (response.status_code == 302):
+                    print(response.url + " redirected to " + str(response.headers['Location']))
+                    response.url = response.headers['Location']
+                    port.setUrl(response.url)
+                else: 
+                    print(response.url + " caused " + response.status_code)
+                    port.setUrl("status code : " + response.status_code)
+            except requests.Timeout:
+                print(single_entry_url + " request timed out")
+                pass
+            except requests.ConnectionError:
+                print(single_entry_url + " url does not resolve")
+                pass
+            except:
+                pass
+        else:
+            port.setUrl("unknown")
 def scanHost(subdomain):
     a = subdomain.name
-    print("starting scan ", subdomain.name)
+    print("Starting scan : ", subdomain.name)
     ip = socket.gethostbyname(a)
     subdomain.setIp(ip)
     tcp_scan(subdomain)
@@ -35,7 +63,7 @@ def tcp_scan(subdomain):
 
     for port in ports_c:
         if(port!=-1):
-            portObj = Port("name","sercive")
+            portObj = Port("name","sercive","url")
             portObj.setPort(port)
             if port!=443:
                 portObj.setService(bannerGrabbing.getService(ip,port))
@@ -43,7 +71,8 @@ def tcp_scan(subdomain):
                 portObj.setService(bannerGrabbing.getService(subdomain.name,443))
             portsObj.append(portObj)
     subdomain.setPorts(portsObj)
-   
+    webProbe(subdomain)
+
 def scan(domain):
     subdomain_list = findSubdomains.findSubdomains(domain,25)
     for subdoms in subdomain_list:
@@ -52,6 +81,7 @@ def scan(domain):
         subdomain.setName(f'{subdoms}')
         scanHost(subdomain)
         for port in subdomain.ports:
+            print(port.url)
             mongo.db.subdomain.insert_one(dict(name=subdomain.name,ip=subdomain.ip,port=port.name,service=port.service))  
  
 @app.route("/")
--- objects/Port.py
+++ objects/Port.py
@@ -1,8 +1,11 @@
 class Port:
-    def __init__(self, name, service):
+    def __init__(self, name, service, url):
         self.name = name
         self.service = service
+        self.url = url
     def setPort(self, name):
         self.name = name
     def setService(self, service):
         self.service = service
+    def setUrl(self, url):
+        self.url = url
